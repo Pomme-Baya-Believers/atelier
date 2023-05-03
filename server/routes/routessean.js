dbd85bf7f821451c1bfe7d4ded4c5ed982a868d2
Sean Winnik <winnik.sean@gmail.com>
2023-05-03 12:22:20 -0700

    adding routes and controllers, imported in server/index.js

---
+++ server/routes/routessean.js
@@ -0,0 +1,8 @@
+const Router = require('express').Router()
+const controllersSean= require('../controllers/controllerssean.js')
+
+
+Router.get('/sean/details', controllersSean.get)
+Router.post
+
+
