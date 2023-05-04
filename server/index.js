9ddeae7fee9007709986567ed3564bf473af256b
Naru Sadakuni <narusadakuni1@gmail.com>
2023-05-04 15:23:43 -0700

    added get request handler to controllers, a route for get requests to router, and imported the router file to index.js

--- server/index.js
+++ server/index.js
@@ -4,9 +4,12 @@ const express = require('express')
 const path = require('path')
 const app = express()
 //import your routes
+const routerNaru = require('./routes/routesnaru.js')
 
 app.use(express.json())
 app.use(express.static('client/dist'))
+
+app.use('/naru', routerNaru)
 
 app.listen(process.env.PORT) || 3000
 console.log('Listening on port '+process.env.PORT)
