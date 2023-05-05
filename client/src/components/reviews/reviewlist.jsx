3fb1c5604141cf9908b0095a94233c9513a3f2eb
Naru Sadakuni <narusadakuni1@gmail.com>
2023-05-05 11:20:49 -0700

    added sorting drop down menu

--- client/src/components/reviews/reviewlist.jsx
+++ client/src/components/reviews/reviewlist.jsx
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import ReviewTile from './reviewtile.jsx';
 import apiHelper from './apihelpers.jsx';
+import Sort from './sort.jsx';
 
 const ReviewList = ({ productID }) => {
   const [reviews, setReviews] = useState({});
   const [count, setCount] = useState(2);
+  const [sort, setSort] = useState('relevant');
 
-  useEffect(() => apiHelper.getReviews(count, 'newest', productID, setReviews), []);
-  useEffect(() => apiHelper.getReviews(count, 'newest', productID, setReviews), [count]);
+  useEffect(() => apiHelper.getReviews(count, sort, productID, setReviews), []);
+  useEffect(() => apiHelper.getReviews(count, sort, productID, setReviews), [count]);
+  useEffect(() => apiHelper.getReviews(count, sort, productID, setReviews), [sort]);
 
   return (
     <div>
+      <Sort setSort={setSort}/>
       {reviews.results
       && reviews.results.map((review) => <ReviewTile key={review.review_id} review={review} />)}
       <button type="button" onClick={() => { setCount(count + 2); }}>More reviews</button>
