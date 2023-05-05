ef2a96553f8dbf7e27fb56f9655ba649c6d18d3b
Darrel Han <darrellh0112@gmail.com>
2023-05-04 22:57:14 -0700

    create questions module and create multiple components

---
+++ client/src/components/questions/SearchQuestions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchQuestions = () => {
+
+  return (
+    <form>
+      <input type='text' name='question' placeholder='HAVE A QUESTION? SEARCH YOUR ANSWER...'></input>
+      <button type='submit'>Search</button>
+    </form>
+  )
+}
+
+export default SearchQuestions;
