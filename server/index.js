3c4293a6d475503d1f005994c8f4c4cf27813fab
Sean Winnik <winnik.sean@gmail.com>
2023-05-05 18:56:19 -0700

    fixing merge conflict

--- server/index.js
+++ server/index.js
@@ -2,36 +2,19 @@ require("dotenv").config();
 
 const express = require('express');
 const path = require('path');
-<<<<<<< HEAD
-
 const app = express();
-// const routerSean = require('./routes/routessean.js')
-// const controllersSean = require('./controllers/controllerssean');
-
-  //import your routes
-const routerNaru = require('./routes/routesnaru.js');
-const routerSean = require('./routes/routessean.js');
-=======
-const app = express();
-// const routerSean = require('./routes/routessean.js')
-// const controllersSean = require('./controllers/controllerssean.js')
 
 //import your routes
 const routerNaru = require('./routes/routesnaru.js');
 const routerMatthew = require('./routes/routesmatthew.js');
->>>>>>> 1164ce342bdd79e8194684890601795baf1d5a95
+const routerSean = require('./routes/routessean.js');
 
 app.use(express.json());
 app.use(express.static('client/dist'));
 
-<<<<<<< HEAD
 app.use('/sean', routerSean);
 app.use('/naru', routerNaru);
-=======
-// app.get('/sean/products',  controllersSean.get);
-// app.use('/naru', routerNaru)
 app.use('/matthew', routerMatthew);
->>>>>>> 1164ce342bdd79e8194684890601795baf1d5a95
 
 app.listen(process.env.PORT) || 3000
 console.log('Listening on port ' + process.env.PORT);
