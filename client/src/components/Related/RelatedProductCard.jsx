530c77dbdcf1af57e10f53e6d1fa5e474e42cdeb
Sean Winnik <winnik.sean@gmail.com>
2023-05-09 09:31:52 -0700

    1.4.2Modal prepping to pull master

--- client/src/components/Related/RelatedProductCard.jsx
+++ client/src/components/Related/RelatedProductCard.jsx
@@ -3,43 +3,37 @@ import axios from 'axios';
 
 const { useState, useEffect } = React;
 const RelatedProductsCard = ({ productID, setProductID }) => {
+  const related = true;
 
-const related = true ;
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [category, setCategory] = useState('');
+  const [id, setID] = useState('');
 
+  useEffect(()=> {
+    axios.get('/sean/products')
+      .then((res) => {
+        setName(res.data.name);
+        setPrice(res.data.default_price);
+        // setCategory(res.data.category)
+      });
+  });
 
-const [name, setName] = useState('')
-const [price, setPrice] = useState('')
-const [category, setCategory] = useState('')
-const [id, setID] = useState('')
+  const cardClick = () => {
+    console.log(productID);
+    setProductID(productID);
+  };
 
+  const actionClick = (e) =>{
+    console.log("ACTION CLICKED")
+  };
 
-useEffect(()=> {
-  axios.get('/sean/products')
-  .then((res) => {
-    setName(res.data.name)
-    setPrice(res.data.default_price )
-    // setCategory(res.data.category)
-  })
-})
-
-
-const cardClick = (e) => {
-  console.log(productID)
-  setProductID(productID)
-}
-
-
-const actionClick = (e) =>{
-  console.log("ACTION CLICKED")
-}
-
-const actionText = related ? '★' : 'X';
-
+  const actionText = related ? '★' : 'X';
 
   return (
     <div className="relatedCard" >
       <div className="relatedProductImage">
-        <div  className="relatedActionButton" onClick={actionClick}> {actionText} </div>
+        <div className="relatedActionButton" onClick={actionClick}> {actionText} </div>
         <img onClick={cardClick} src="https://cdn.shopify.com/s/files/1/0419/1525/products/1024x1024-Men-Captain-Tobacco-043021-2.jpg?v=1620400973">
         </img>
       </div>
@@ -50,8 +44,8 @@ const actionText = related ? '★' : 'X';
         <div className="relatedStars"> ★★★★★  </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default RelatedProductsCard;
 
