87959d55130bf54154cbe652f8aee3863e363a32
Naru Sadakuni <narusadakuni1@gmail.com>
2023-05-11 20:15:41 -0700

    filtering by rating compelted

--- client/src/components/reviews/reviewlist.jsx
+++ client/src/components/reviews/reviewlist.jsx
@@ -11,6 +11,7 @@ const ReviewList = ({ productID }) => {
   const [count, setCount] = useState(2);
   const [sort, setSort] = useState('relevant');
   const [meta, setMeta] = useState([]);
+  const [displayedReviews, setDisplayedReviews] = useState([]);
 
   useEffect(() => apiHelper.getReviews(10000, sort, productID, setReviews), []);
   useEffect(() => apiHelper.getReviews(10000, sort, productID, setReviews), [sort]);
@@ -20,16 +21,17 @@ const ReviewList = ({ productID }) => {
     <div>
       <div id='reviewComponent'>
         <div id='breakdowns'>
-          <RatingBreakdown meta={meta}/>
+          <RatingBreakdown meta={meta} reviews={reviews}
+          setDisplayedReviews={setDisplayedReviews}/>
           <ProductBreakdown meta={meta}/>
         </div>
         <div id='reviewAllTiles'>
-        <Sort setSort={setSort} reviews={reviews}/>
+        <Sort setSort={setSort} displayedReviews={displayedReviews}/>
         <NewReview productID={productID} meta={meta}/>
-        {reviews.results && reviews.results.slice(0, count).map((review) => <ReviewTile
+        {displayedReviews && displayedReviews.slice(0, count).map((review) => <ReviewTile
         key={review.review_id} review={review}/>)}
           <div className='buttons'>
-            {reviews.results && count <= reviews.results.length && <button className='reviewButton' type="button" onClick={() => { setCount(count + 2); }}>More reviews</button>}
+            {displayedReviews && count < displayedReviews.length && <button className='reviewButton' type="button" onClick={() => { setCount(count + 2); }}>More reviews</button>}
             <button className='newReviewButton' type="button" onClick={() => { document.getElementById('newReview').showModal(); }} >Write a review</button>
           </div>
         </div>
--- client/src/components/reviews/sort.jsx
+++ client/src/components/reviews/sort.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Sort = ({ setSort, reviews }) => (
-  <div id='sortComponent'>{reviews.results && reviews.results.length} reviews, sorted by
+const Sort = ({ setSort, displayedReviews }) => (
+  <div id='sortComponent'>{displayedReviews && displayedReviews.length} reviews, sorted by
   <select name='Sort By' id='sortMenu' onChange={(e) => setSort(e.target.value)}>
     <option value='relevant'>relevance</option>
     <option value='newest'>newest</option>
