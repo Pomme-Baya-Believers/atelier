97bd79a83f6ca53c4b573299dbcc0bbd1a5b1ca6
Sean Winnik <winnik.sean@gmail.com>
2023-05-19 10:55:36 -0700

    detailsModal LightMode legibility

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -62,12 +62,12 @@ const App = () => {
   return (
     <div>
       <div id='appHeaderMargin'>
-      <header id='appHeader'>Atelier
-        <a className='membersTop' href='https://www.linkedin.com/in/matthew-baseman/'>Matthew Baseman</a>
-        <a className='membersTop' href='https://www.linkedin.com/in/sean-winnik-9644aa47/'>Sean Winnik</a>
-        <a className='membersTop' href='https://www.linkedin.com/in/naru-sadakuni-0a402310a/'>Naru Sadakuni</a>
-        <button id="darkButton" onClick={() => setTheme(!theme)}>{theme ? 'Light mode' : 'Dark mode'}</button>
-      </header>
+        <header id='appHeader'>Atelier
+          <a className='membersTop' href='https://www.linkedin.com/in/matthew-baseman/'>Matthew Baseman</a>
+          <a className='membersTop' href='https://www.linkedin.com/in/sean-winnik-9644aa47/'>Sean Winnik</a>
+          <a className='membersTop' href='https://www.linkedin.com/in/naru-sadakuni-0a402310a/'>Naru Sadakuni</a>
+          <button id="darkButton" onClick={() => setTheme(!theme)}>{theme ? 'Light mode' : 'Dark mode'}</button>
+        </header>
       </div>
       <div id='appHeaderFiller'></div>
       <div id='backdrop'></div>
